f8254885530f39c753db7a6cb33f3860dcf1310e
momentum-equation <surajkoolkarni@yahoo.com>
2020-09-24 16:13:28 +0530

    modified:   foamTimeAverage.C

--- foamTimeAverage.C
+++ foamTimeAverage.C
@@ -1,26 +1,25 @@
 /*---------------------------------------------------------------------------*\
- *=========                 |
- *\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
- *\\     /   O peration     |
- *\\    /    A nd           | Copyright (C) 1991-2009 OpenCFD Ltd.
- *\\   /     M anipulation  |
- * -------------------------------------------------------------------------------
- license
- This file is part of OpenFOAM.
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2012-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
 
- OpenFOAM is free software; you can redistribute it and/or modify it
- under the terms of the GNU General Public License as published by the
- Free Software Foundation; either version 2 of the License, or (at your
- option) any later version.
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
- OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- for more details.
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
 
- You should have received a copy of the GNU General Public License
- along with OpenFOAM; if not, write to the Free Software Foundation,
- Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 Application
    foamTimeAverage
@@ -28,6 +27,10 @@ Application
 Author
   Xiaoping Qiu
   q.giskard@gmail.com
+
+Modified for OF-5.x by:
+  Suraj Kulkarni
+  surajkoolkarni@yahoo.com    
 
 Description
 Calculates the time average  of the specified volField over the specified time range.
